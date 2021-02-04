86133eb382a750ec6459bd889481c439d438e175
Evgeny <salabaikin@mail.ru>
2021-02-05 00:19:05 +0300

    Сделал первую страницу. адаптировал

---
+++ dist/js/app.js
@@ -0,0 +1,92 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+  const promoSwiper = new Swiper('.promo-slider.swiper-container', {
+    loop: false,
+    breakpoints: {
+      768: {
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      },
+    }
+  });
+  const companySwiper = new Swiper('.company-slider.swiper-container', {
+    loop: true,
+    autoplay: {
+      delay: 3500,
+    },
+    navigation: false
+  });
+
+  // Modal 
+  const popUp = () => {
+    const modalContent = document.querySelector('.modal__content');
+
+    const modalData = {
+      popupLink: document.querySelectorAll('[data-link-id]'),
+      modal: document.querySelectorAll('.modal'),
+      closeBtn(elem) {
+        const closeBtn = elem.querySelector('.modal-close');
+        closeBtn.addEventListener('click', () => {
+          this.closeModal(elem);
+        });
+        this.modal.forEach(elem => {
+          elem.addEventListener('click', (e) => {
+            if (e.target.classList.contains('modal__window')) {
+              this.closeModal(elem);
+            }
+          });
+        });
+
+      },
+      closeModal(elem) {
+        elem.classList.remove('modal-open');
+      },
+      openModal(attr) {
+        this.modal.forEach((elem) => {
+          const elemAttr = elem.getAttribute('data-modal-id');
+          if (attr === elemAttr) {
+            elem.classList.add('modal-open');
+            this.closeBtn(elem);
+          }
+        });
+      },
+    };
+
+    modalData.popupLink.forEach(elem => {
+      elem.addEventListener('click', (event) => {
+        event.preventDefault();
+        const target = event.target;
+        const attrLinkValue = target.getAttribute('data-link-id');
+        modalData.openModal(attrLinkValue);
+      });
+    });
+
+  };
+
+  popUp();
+
+  // Menu
+  const header = document.querySelector('.header'),
+        headerNav = header.querySelector('.header__nav'),
+        overlay = document.querySelector('.overlay');
+
+  header.addEventListener('click', (e) => {
+    const target = e.target;
+
+    if (target.closest('.burger')) {
+      headerNav.classList.toggle('active');
+      overlay.classList.toggle('active');
+    } else if (target.closest('.header__close')) {
+      headerNav.classList.remove('active');
+      overlay.classList.remove('active');
+    }
+  });
+
+  overlay.addEventListener('click', () => {
+    headerNav.classList.remove('active');
+    overlay.classList.remove('active');
+  })
+
+});
